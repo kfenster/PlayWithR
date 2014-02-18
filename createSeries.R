fd5213c836b45fd8a036ecfc93ee0efd9f804312
Kurt Fenstermacher <kfenstermacher@gmail.com>
2014-02-17 22:20:38 -0500

    build out mean (tsWithMean) and trend-adding series

--- createSeries.R
+++ createSeries.R
@@ -1,4 +1,18 @@
 require(xts)
+
+tsWithMean <- function(tsMean=0,tsLen=100, tsMin=-20, startDate='2000-01-01') {
+  return(xts(runif(tsLen,tsMin,-tsMin)+tsMean,as.Date(startDate)+1:tsLen))
+}
+
+addTrend <- function(timeSeries, addFactor=1) {
+  return (cumsum(rep(addFactor,length(timeSeries)))+timeSeries)
+}
+
+multTrend <- function(timeSeries, multFactor=0.05) {
+  return (cumprod(rep((1+multFactor),length(timeSeries)))+timeSeries)
+}
+  
+
 data(sample_matrix)
 class(sample_matrix)
 
@@ -6,3 +20,13 @@ class(sample_matrix)
 simpleSeries <- seq(1,500,3)
 simpleTimeSeries <- ts(simpleSeries,start=c(1970,1), frequency=12)
 simpleTimeSeries1980 <- ts(simpleSeries,start=c(1980,1), frequency=12)
+xtsSampleHourly <- xts(seq(100,5000,10)+runif(491,-20,20), Sys.Date()+1:491+runif(491,-20,20)/10)
+dailySample <- to.daily(xtsSampleHourly)
+dailySampleUni <- dailySample[,4]
+
+intercept <- 134.056
+trend_slope <- 1.07345
+seasonal
+
+require(forecast)
+
