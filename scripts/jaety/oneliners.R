bc08802c32e192c139e2837309d75b9811fdeebd
Jaety Edwards <jaetye@gmail.com>
2014-03-08 10:25:58 -0500

    example of plotting in 3D

--- scripts/jaety/oneliners.R
+++ scripts/jaety/oneliners.R
@@ -7,3 +7,9 @@ rowSums(tbl) / sum(tbl)
 # but are students more likely to default?
 # p(student | default = YES) vs p(~student | default = YES)
 (tbl / colSums(tbl))["Yes",  , drop=FALSE]
+
+# plotting a multivariate normal distribution
+library(MASS)
+library(rgl)
+data = mvrnorm(10000, rep(0,3), diag(c(1,2,1)))
+plot3d(data[,1], data[,2], data[,3], aspect="iso")
